65e924dc2aca484b8ee3269749fcf3e09cc6a38a
azhar_azad <azhar.u.azad@gmail.com>
2022-05-20 20:50:28 +0600

    cineplex2 project initiated

---
+++ practice/cineplex2/src/main/java/com/azad/cineplex2/Cineplex2Application.java
@@ -0,0 +1,13 @@
+package com.azad.cineplex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cineplex2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cineplex2Application.class, args);
+	}
+
+}
---
+++ practice/cineplex2/src/test/java/com/azad/cineplex2/Cineplex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.cineplex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cineplex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
