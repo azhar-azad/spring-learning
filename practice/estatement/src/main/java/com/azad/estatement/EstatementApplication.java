74632b3d85327dab594e030790cff5bfd805fe33
azhar_azad <azhar.u.azad@gmail.com>
2022-05-25 00:39:59 +0600

    estatement project init and db configured

---
+++ practice/estatement/src/main/java/com/azad/estatement/EstatementApplication.java
@@ -0,0 +1,13 @@
+package com.azad.estatement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstatementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstatementApplication.class, args);
+	}
+
+}
