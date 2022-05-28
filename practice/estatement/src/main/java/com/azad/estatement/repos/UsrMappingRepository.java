f946bdcb47dba426b9068afe6fdb9f3b8da13aba
azhar_azad <azhar.u.azad@gmail.com>
2022-05-29 01:32:15 +0600

    estmt: usr_mapping repo methods added

--- practice/estatement/src/main/java/com/azad/estatement/repos/UsrMappingRepository.java
+++ practice/estatement/src/main/java/com/azad/estatement/repos/UsrMappingRepository.java
@@ -1,5 +1,7 @@
 package com.azad.estatement.repos;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.Modifying;
@@ -13,6 +15,13 @@ import com.azad.estatement.models.entities.UsrMappingEntity;
 @Repository
 public interface UsrMappingRepository extends PagingAndSortingRepository<UsrMappingEntity, Long> {
 	
+	List<UsrMappingEntity> findByCifNum(String cifNum);
+	
+	UsrMappingEntity findByUsrId(Long usrId);
+	
 	@Transactional
 	Long deleteByUsrId(Long usrId);
+	
+	@Transactional
+	Long deleteByCifNum(String cifNum);
 }
