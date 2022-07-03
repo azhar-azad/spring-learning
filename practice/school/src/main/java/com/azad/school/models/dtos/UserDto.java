478266aeee6b656b7d1fb94563aea3358225d861
azhar_azad <azhar.u.azad@gmail.com>
2022-07-03 18:26:37 +0600

    refactored id field

--- practice/school/src/main/java/com/azad/school/models/dtos/UserDto.java
+++ practice/school/src/main/java/com/azad/school/models/dtos/UserDto.java
@@ -4,16 +4,16 @@ import com.azad.school.models.User;
 
 public class UserDto extends User {
 
-    private Long id;
+    private Long userId;
 
     public UserDto() {
     }
 
-    public Long getId() {
-        return id;
+    public Long getUserId() {
+        return userId;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 }
