ec06162559db88514a84cba10ab7bd44d8526f2e
azhar_azad <azhar.u.azad@gmail.com>
2022-07-10 19:08:37 +0600

    boilerplate configurations added

---
+++ practice/online-ed/src/main/java/com/azad/onlineed/responses/ApiResponse.java
@@ -0,0 +1,45 @@
+package com.azad.onlineed.responses;
+
+import java.util.List;
+import java.util.Map;
+
+public class ApiResponse {
+
+    private Boolean success;
+    private String message;
+
+    private Map<String, List<?>> data;
+
+    public ApiResponse() {
+    }
+
+    public ApiResponse(Boolean success, String message, Map<String, List<?>> data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, List<?>> getData() {
+        return data;
+    }
+
+    public void setData(Map<String, List<?>> data) {
+        this.data = data;
+    }
+}
