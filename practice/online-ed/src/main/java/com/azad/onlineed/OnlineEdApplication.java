18c016f4457163243dddfa8b7c78178b08363037
azhar_azad <azhar.u.azad@gmail.com>
2022-07-10 18:52:40 +0600

    online-ed project initialized

---
+++ practice/online-ed/src/main/java/com/azad/onlineed/OnlineEdApplication.java
@@ -0,0 +1,13 @@
+package com.azad.onlineed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineEdApplication.class, args);
+	}
+
+}
---
+++ practice/online-ed/src/test/java/com/azad/onlineed/OnlineEdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.onlineed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineEdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
