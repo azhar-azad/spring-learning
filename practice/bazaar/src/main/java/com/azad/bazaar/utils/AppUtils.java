a084fcdd085d536ab0f7b0669b49db931a1ec598
azhar_azad <azhar.u.azad@gmail.com>
2022-07-14 18:24:53 +0600

    app boilerplate added

---
+++ practice/bazaar/src/main/java/com/azad/bazaar/utils/AppUtils.java
@@ -0,0 +1,65 @@
+package com.azad.bazaar.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class AppUtils {
+
+    private final Logger LOG = LoggerFactory.getLogger(AppUtils.class);
+
+    public void printControllerMethodInfo(String httpMethod, String requestPath, String controllerMethodName, boolean isPublic, String authorizedFor) {
+        LOG.info("");
+        LOG.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        LOG.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        LOG.info(httpMethod + " " + requestPath + " :::: " + controllerMethodName);
+        if (isPublic)
+            LOG.info("PUBLIC :: No Authentication");
+        else {
+            LOG.info("PRIVATE :: Authenticated & Authorized for {}", authorizedFor);
+        }
+        LOG.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        LOG.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    }
+
+    public Sort getSortBy(String sort, String order) {
+
+        String[] sortItems = sort.split(",");
+        String[] filteredSortItems = new String[sortItems.length];
+
+        for (int i = 0; i < sortItems.length; i++) {
+            filteredSortItems[i] = sortItems[i].trim();
+        }
+
+        Sort sortBy = Sort.by(filteredSortItems);
+
+        if (order.equalsIgnoreCase("desc")) {
+            sortBy = sortBy.descending();
+        } else {
+            sortBy = sortBy.ascending();
+        }
+
+        return sortBy;
+    }
+
+    private String getRandomString() {
+
+        int leftLimit= 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 8;
+
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <=90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+}
