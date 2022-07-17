cb9be7fc3c8289384d121d23011e8c46c9ed0c66
azhar_azad <azhar.u.azad@gmail.com>
2022-07-17 17:20:46 +0600

    jsonplaceholder clone project bootstraped

---
+++ practice/jsonplaceholder-clone/src/main/java/com/azad/jsonplaceholderclone/JsonplaceholderCloneApplication.java
@@ -0,0 +1,13 @@
+package com.azad.jsonplaceholderclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonplaceholderCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonplaceholderCloneApplication.class, args);
+	}
+
+}
---
+++ practice/jsonplaceholder-clone/src/test/java/com/azad/jsonplaceholderclone/JsonplaceholderCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.jsonplaceholderclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonplaceholderCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
