88543365eae13d6bbcb24f0bb08f9dfc34d6b5bc
azhar_azad <azhar.u.azad@gmail.com>
2022-07-23 23:05:05 +0600

    configs, exceptions, utils and security configured

---
+++ practice/jsonplaceholder-clone/src/main/java/com/azad/jsonplaceholderclone/security/auth/MemberDetailsService.java
@@ -0,0 +1,25 @@
+package com.azad.jsonplaceholderclone.security.auth;
+
+import com.azad.jsonplaceholderclone.repos.MemberRepository;
+import com.azad.jsonplaceholderclone.security.entities.MemberEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberDetailsService implements UserDetailsService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        MemberEntity memberEntity = memberRepository.findByUsername(username).orElseThrow(
+                () -> new RuntimeException("User not found with username: " + username));
+
+        return new MemberDetails(memberEntity);
+    }
+}
