663ff8a319a5c78aab26306c64b4eaa8764c5065
azhar_azad <azhar.u.azad@gmail.com>
2022-07-31 19:52:43 +0600

    hateoas tutorial init

---
+++ tutorials/codejava.net/hateoas-crud/src/main/java/com/azad/hateoascrud/HateoasCrudApplication.java
@@ -0,0 +1,13 @@
+package com.azad.hateoascrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HateoasCrudApplication.class, args);
+	}
+
+}
---
+++ tutorials/codejava.net/hateoas-crud/src/test/java/com/azad/hateoascrud/HateoasCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.hateoascrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
