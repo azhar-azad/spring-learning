c9d445cbe5dbb50cabc39beb8cbcfd86639a4883
azhar_azad <azhar.u.azad@gmail.com>
2022-08-28 04:18:35 +0600

    ListShare project initialized

---
+++ practice/ListShare/src/main/java/com/azad/ListShare/ListShareApplication.java
@@ -0,0 +1,13 @@
+package com.azad.ListShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListShareApplication.class, args);
+	}
+
+}
---
+++ practice/ListShare/src/test/java/com/azad/ListShare/ListShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.ListShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
