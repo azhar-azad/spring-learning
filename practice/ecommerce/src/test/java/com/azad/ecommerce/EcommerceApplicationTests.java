3f6ae77bed762753ae0f7982d50b9781a22349bc
azhar_azad <azhar.u.azad@gmail.com>
2022-09-03 21:35:04 +0600

    ecommerce project initiated

---
+++ practice/ecommerce/src/test/java/com/azad/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
