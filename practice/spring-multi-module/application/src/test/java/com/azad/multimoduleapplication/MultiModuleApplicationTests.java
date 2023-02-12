55b8b5ac15974d28868a90f2ab988fe508df28e2
azhar_azad <azhar.u.azad@gmail.com>
2023-02-13 01:58:39 +0600

    multi-module spring project done

---
+++ practice/spring-multi-module/application/src/test/java/com/azad/multimoduleapplication/MultiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.multimoduleapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ practice/spring-multi-module/library/src/test/java/com/azad/multimodulelibrary/MultiModuleLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.multimodulelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
