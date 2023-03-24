68a9724fc599ce592277c7cd27007c30faef9ed3
azhar_azad <azhar.u.azad@gmail.com>
2023-03-24 16:23:29 +0600

    multi-module project done~

---
+++ practice/hospital-mgmt-system-mm/repository/src/main/java/com/azad/dao/doctor/DoctorRepository.java
@@ -0,0 +1,9 @@
+package com.azad.dao.doctor;
+
+import com.azad.model.doctor.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+}
