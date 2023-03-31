6f90fb2e415a713a493726b37922aee692935de4
azhar_azad <azhar.u.azad@gmail.com>
2023-04-01 01:02:21 +0600

    Generic model assembler documentation added

--- practice/stackoverflow-clone/web/src/main/java/com/azad/web/assemblers/GenericApiResponseModelAssembler.java
+++ practice/stackoverflow-clone/web/src/main/java/com/azad/web/assemblers/GenericApiResponseModelAssembler.java
@@ -6,6 +6,32 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 
+/***
+ * Generic response model assembler.
+ *      toModel:
+ *          - Receive a response class.
+ *          - Convert to EntityModel and return.
+ *      toCollectionModel:
+ *          - Receive an Iterable of responses.
+ *          - Convert each response to EntityModel and add to a CollectionModel.
+ *          - Return the CollectionModel.
+ *      getCollectionModel:
+ *          - Receive an Iterable of responses and the paging and sorting data.
+ *          - Convert to CollectionModel using toCollectionModel method.
+ *          - Add PREV and NEXT links using the addNextLink and addPrevLink methods. (if applicable)
+ *          - Return the CollectionModel.
+ *      addNextLink:
+ *          - Receive a CollectionModel and paging and sorting data.
+ *          - Add NEXT link if applicable.
+ *          - Return the CollectionModel.
+ *      addPrevLink:
+ *          - Receive a CollectionModel and paging and sorting data.
+ *          - Add PREV link if applicable.
+ *          - Return the CollectionModel.
+ *
+ * @param <T> Entity response class.
+ */
+
 public interface GenericApiResponseModelAssembler<T> extends RepresentationModelAssembler<T, EntityModel<T>> {
 
     @Value("${default_page_number}")
@@ -15,9 +41,9 @@ public interface GenericApiResponseModelAssembler<T> extends RepresentationModel
     @Value("${default_sort_order}")
     String defaultOrder = "asc";
 
-    EntityModel<T> toModel(T entity);
-    CollectionModel<EntityModel<T>> toCollectionModel(Iterable<? extends T> entities);
-    CollectionModel<EntityModel<T>> getCollectionModel(Iterable<? extends T> entities, PagingAndSorting ps);
+    EntityModel<T> toModel(T response);
+    CollectionModel<EntityModel<T>> toCollectionModel(Iterable<? extends T> responses);
+    CollectionModel<EntityModel<T>> getCollectionModel(Iterable<? extends T> responses, PagingAndSorting ps);
     void addNextLink(CollectionModel<EntityModel<T>> collectionModel, PagingAndSorting ps);
     void addPrevLink(CollectionModel<EntityModel<T>> collectionModel, PagingAndSorting ps);
 }
