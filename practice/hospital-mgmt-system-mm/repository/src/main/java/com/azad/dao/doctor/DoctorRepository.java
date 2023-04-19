03031b90c738a4d9bfe4d3b7caeffae70e6cc32f
azhar_azad <azhar.u.azad@gmail.com>
2023-04-19 23:52:23 +0600

    hospital app pruned

--- practice/hospital-mgmt-system-mm/repository/src/main/java/com/azad/dao/doctor/DoctorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.azad.dao.doctor;
-
-import com.azad.model.doctor.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-}
