d464797b49db95415faf3fd3fa47c2af0d99d915
azhar_azad <azhar.u.azad@gmail.com>
2023-07-17 21:17:18 +0600

    simple-project-management entity design done

---
+++ spring-boot3/simple-project-management/src/main/java/com/azad/simpleprojectmanagement/SimpleProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.azad.simpleprojectmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleProjectManagementApplication.class, args);
+	}
+
+}
---
+++ spring-boot3/simple-project-management/src/test/java/com/azad/simpleprojectmanagement/SimpleProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.simpleprojectmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
