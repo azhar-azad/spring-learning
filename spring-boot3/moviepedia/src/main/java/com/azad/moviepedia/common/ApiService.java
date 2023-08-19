0b0036df2af8d59304f001d137b8b12dbf8d87aa
azhar_azad <azhar.u.azad@gmail.com>
2023-08-19 18:24:29 +0600

    common modules done. closes #8

---
+++ spring-boot3/moviepedia/src/main/java/com/azad/moviepedia/common/ApiService.java
@@ -0,0 +1,12 @@
+package com.azad.moviepedia.common;
+
+import java.util.List;
+
+public interface ApiService<T> {
+
+    T create(T dto);
+    List<T> getAll(PagingAndSorting ps);
+    T getById(Long id);
+    T updateById(Long id, T updatedDto);
+    void deleteById(Long id);
+}
