1d8839fcdd6943301e99eb37bfd436ab5378e717
azhar_azad <azhar.u.azad@gmail.com>
2023-08-19 17:18:59 +0600

    moviepedia api initialized

---
+++ spring-boot3/moviepedia/src/main/java/com/azad/moviepedia/MoviepediaApplication.java
@@ -0,0 +1,13 @@
+package com.azad.moviepedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviepediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviepediaApplication.class, args);
+	}
+
+}
