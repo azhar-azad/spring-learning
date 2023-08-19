b6c7fcb5efd92e4a66ed79d35b8e300ac951fd8c
azhar_azad <azhar.u.azad@gmail.com>
2023-08-19 20:35:32 +0600

    security implemented. closes #4

--- spring-boot3/moviepedia/src/main/java/com/azad/moviepedia/repositories/MemberRepository.java
+++ spring-boot3/moviepedia/src/main/java/com/azad/moviepedia/repositories/MemberRepository.java
@@ -4,6 +4,9 @@ import com.azad.moviepedia.models.auth.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    Optional<MemberEntity> findByEmail(String email);
 }
