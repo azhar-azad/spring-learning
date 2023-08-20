42e79b2ad23f3af68d5b4dfb77a10e9cdf854149
azhar_azad <azhar.u.azad@gmail.com>
2023-08-21 01:43:47 +0600

    register and login done. closes #11

--- spring-boot3/moviepedia/src/main/java/com/azad/moviepedia/repositories/MemberInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.azad.moviepedia.repositories;
-
-import com.azad.moviepedia.models.memberinfo.MemberInfoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberInfoRepository extends JpaRepository<MemberInfoEntity, Long> {
-}
