d8e2c214b82179552167f231f445015bb834a5c4
azhar_azad <azhar.u.azad@gmail.com>
2023-12-20 13:38:37 +0600

    multiplex boilerplate added

---
+++ spring-boot3/multiplex/src/main/java/com/azad/multiplex/common/ApiRestController.java
@@ -0,0 +1,14 @@
+package com.azad.multiplex.common;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+
+public interface ApiRestController<REQ, RES> {
+
+    ResponseEntity<EntityModel<RES>> createEntity(REQ request);
+    ResponseEntity<CollectionModel<EntityModel<RES>>> getAllEntity(int page, int limit, String sort, String order);
+    ResponseEntity<EntityModel<RES>> getEntity(Long id);
+    ResponseEntity<EntityModel<RES>> updateEntity(Long id, REQ updatedRequest);
+    ResponseEntity<?> deleteEntity(Long id);
+}
---
+++ spring-boot3/multiplex/src/test/java/com/azad/multiplex/MultiplexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.multiplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
