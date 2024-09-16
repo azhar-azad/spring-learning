eab0f502ba4a031df67ec6f4c0d9a6ac9305fecf
azhar-azad <azhar.u.azad@gmail.com>
2024-09-17 00:52:47 +0600

    tennis project started

---
+++ spring-vue/tennis-scoreboard/tennis-score-api/src/main/java/com/azad/tennis_score_api/TennisScoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.azad.tennis_score_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisScoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisScoreApiApplication.class, args);
+	}
+
+}
---
+++ spring-vue/tennis-scoreboard/tennis-score-api/src/test/java/com/azad/tennis_score_api/TennisScoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.tennis_score_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisScoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
